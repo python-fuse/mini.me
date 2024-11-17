d4748eb9b1cec3e8b8e131e749d55d99062233bf
pyfuse <hauwamuktar880@gmail.com>
2024-11-17 21:58:20 +0100

    removed unused imports

--- src/app/dashboard/links/page.tsx
+++ src/app/dashboard/links/page.tsx
@@ -1,10 +1,8 @@
 import { Divider } from "@mui/material";
 import LinksTitleRow from "./_components/LinksTitleRow";
 import AllLinks from "./_components/AllLinks";
-import { getServerSession } from "next-auth";
 
 const page = async () => {
-  const session = await getServerSession();
   return (
     <div className="px-20 py-10 flex flex-col gap-y-2 overflow-y-scroll">
       {/* LinkPage Title row  */}
