d7ad71fada427572baa943de820453999012c18e
pyfuse <hauwamuktar880@gmail.com>
2024-12-25 09:19:21 +0100

    Revert "refactor dashboard components; implement session handling and loading state for improved user experience"
    This reverts commit 571298c50429375a7cfd653da9554ec40144172a.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,95 +1,66 @@
-'use client';
-
-import { useState, useEffect } from 'react';
 import { getUserTotalCLicks, getUserTotalLinks } from '@/src/actions/actions';
 import MyButton from '@/src/components/global/Button';
 import DashboardAnalyticCard from '@/src/components/dashboard/DashboardAnalyticCard';
 import RecentLinks from '@/src/components/dashboard/RecentLinks';
 import Welcome from '@/src/components/dashboard/Welcome';
 import { Divider } from '@mui/material';
+import { getServerSession } from 'next-auth';
+import Link from 'next/link';
 import { BiLink, BiPointer, BiQr } from 'react-icons/bi';
 import DashboardAnalyticSkeleton from '@/src/components/dashboard/DashboardAnalyticSkeleton';
-import { getSession } from 'next-auth/react';
-import { Session } from 'next-auth';
 
-const Page = () => {
-  const [session, setSession] = useState<Session | null>(null);
-  const [loading, setLoading] = useState(true);
-  const [totalUserLinks, setTotalUserLinks] = useState(0);
-  const [totalUserClicks, setTotalUserClicks] = useState(0);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-
-      const session = await getSession();
-      setSession(session);
-
-      if (session?.user?.id) {
-        const [links, clicks] = await Promise.all([
-          getUserTotalLinks(session.user.id),
-          getUserTotalCLicks(session.user.id),
-        ]);
-        setTotalUserLinks(links ?? 0);
-        setTotalUserClicks(clicks ?? 0);
-      }
-
-      setLoading(false);
-    };
-
-    fetchData();
-  }, []);
-
-  if (loading) {
-    return (
-      <div className="px-4 md:px-20 py-4 md:py-10 flex flex-col gap-y-4 overflow-y-scroll">
-        <Welcome session={session} />
-        <div className="flex w-full  gap-x-2 ">
-          <DashboardAnalyticSkeleton />
-          <DashboardAnalyticSkeleton />
-          <DashboardAnalyticSkeleton />
-        </div>
-        <Divider className="bg-tertiary-500" />
-        <RecentLinks />
-      </div>
-    );
-  }
+const page = async () => {
+  const session = await getServerSession();
 
   if (!session) {
-    return <div>Please log in to view your dashboard.</div>;
+    return;
   }
+
+  const totalUserLinks = await getUserTotalLinks(session?.user.id);
+  const totalUserClicks = await getUserTotalCLicks(session?.user.id);
 
   return (
     <div className="px-4 md:px-20 py-4 md:py-10 flex flex-col gap-y-4 overflow-y-scroll">
-      <Welcome session={session} />
+      <Welcome />
       <div className="flex gap-x-2">
-        <DashboardAnalyticCard
-          icon={<BiLink size={22} />}
-          title="Links created"
-          value={totalUserLinks}
-        />
-        <DashboardAnalyticCard
-          icon={<BiQr size={22} />}
-          title="QR codes created"
-          value={totalUserLinks}
-        />
-        <DashboardAnalyticCard
-          icon={<BiPointer size={22} />}
-          title="Total clicks"
-          value={totalUserClicks}
-        />
+        {!session ? (
+          <>
+            <DashboardAnalyticSkeleton />
+            <DashboardAnalyticSkeleton />
+            <DashboardAnalyticSkeleton />
+          </>
+        ) : (
+          <>
+            <DashboardAnalyticCard
+              icon={<BiLink size={22} />}
+              title="Links created"
+              value={totalUserLinks ?? 0}
+            />
+            <DashboardAnalyticCard
+              icon={<BiQr size={22} />}
+              title="QR codes created"
+              value={totalUserLinks ?? 0}
+            />
+            <DashboardAnalyticCard
+              icon={<BiPointer size={22} />}
+              title="Total clicks"
+              value={totalUserClicks ?? 0}
+            />
+          </>
+        )}
       </div>
 
       <Divider className="bg-tertiary-500" />
 
       <div className="flex justify-between">
         <h1 className="text-3xl font-semibold">Recent links</h1>
-        <MyButton>View all</MyButton>
+        <Link href="/dashboard/links">
+          <MyButton>View all</MyButton>
+        </Link>
       </div>
 
       <RecentLinks />
     </div>
   );
 };
-
-export default Page;
+export default page;
--- src/components/dashboard/Welcome.tsx
+++ src/components/dashboard/Welcome.tsx
@@ -1,10 +1,12 @@
-const Welcome = ({ session }: { session: any }) => {
+import { getServerSession } from 'next-auth';
+
+const Welcome = async () => {
+  const session = await getServerSession();
+
   return (
     <div className="border border-tertiary-500 p-10 flex bg-white flex-col bg">
       <p className="text-lg tracking-widest uppercase">Welcome</p>
-      <h1 className="text-4xl font-semibold ">
-        {session?.user?.name ?? '---'}
-      </h1>
+      <h1 className="text-4xl font-semibold ">{session?.user?.name}</h1>
     </div>
   );
 };
